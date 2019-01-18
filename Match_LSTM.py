39d989480839abef1cfa01dabccfe893bc5c0173
ThanhChinhBK <nguyenthanhchinh96@gmail.com>
2019-01-19 02:13:03 +0700

    edit st

--- Match_LSTM.py
+++ Match_LSTM.py
@@ -10,6 +10,206 @@ rnnact_dict = {
 }
 
 
+class Encoder(object):
+
+    # tf.contrib.layers.xavier_initializer):
+    def __init__(self, hidden_size, dropout, initializer=lambda: None):
+        self.hidden_size = hidden_size
+        self.init_weights = initializer
+        self.dropout = dropout
+
+    def encode(self, inputs, masks, encoder_state_input=None):
+        """
+        :param inputs: vector representations of question and hypothesis (a tuple) 
+        :param masks: masking sequences for both question and hypothesis (a tuple)
+        :param encoder_state_input: (Optional) pass this as initial hidden state
+                                    to tf.nn.dynamic_rnn to build conditional representations
+        :return: an encoded representation of the question and hypothesis.
+        """
+
+        question, hypothesis = inputs
+        masks_question, masks_hypothesis = masks
+        masks_question = tf.reduce_sum(masks_question, -1)
+        masks_hypothesis = tf.reduce_sum(masks_hypothesis, -1)
+
+        # read hypothesis conditioned upon the question
+        with tf.variable_scope("encoded_question"):
+            lstm_cell_question = tf.contrib.rnn.BasicLSTMCell(
+                self.hidden_size, state_is_tuple=True)
+            # encoded_question, (q_rep, _) = tf.nn.dynamic_rnn(
+            # lstm_cell_question, question, masks_question, dtype=tf.float32)
+            # # (-1, Q, H)
+            (encoded_question_f, encoded_question_b), ((q_rep_f, _), (q_rep_b, _)) = tf.nn.bidirectional_dynamic_rnn(
+                lstm_cell_question, lstm_cell_question, question, masks_question, dtype=tf.float32)
+            encoded_question = tf.concat(
+                (encoded_question_f, encoded_question_b), -1)  # (-1, P, 2*H)
+            q_rep = tf.concat((q_rep_f, q_rep_b), -1)
+        with tf.variable_scope("encoded_hypothesis"):
+            lstm_cell_hypothesis = tf.contrib.rnn.BasicLSTMCell(
+                self.hidden_size, state_is_tuple=True)
+            # encoded_hypothesis, (p_rep, _) = tf.nn.dynamic_rnn(
+            # lstm_cell_hypothesis, hypothesis, masks_hypothesis,
+            # dtype=tf.float32)  # (-1, P, H)
+            (encoded_hypothesis_f, encoded_hypothesis_b), ((p_rep_f, _), (p_rep_b, _)) = tf.nn.bidirectional_dynamic_rnn(
+                lstm_cell_hypothesis, lstm_cell_hypothesis, hypothesis, masks_hypothesis, dtype=tf.float32)
+            encoded_hypothesis = tf.concat(
+                (encoded_hypothesis_f, encoded_hypothesis_b), -1)  # (-1, P, 2*H)
+            p_rep = tf.concat((p_rep_f, p_rep_b), -1)
+
+        # outputs beyond sequence lengths are masked with 0s
+        encoded_question = tf.tanh(
+            tf.layers.batch_normalization(encoded_question))
+        encoded_hypothesis = tf.tanh(
+            tf.layers.batch_normalization(encoded_hypothesis))
+        q_rep = tf.tanh(tf.layers.batch_normalization(q_rep))
+        p_rep = tf.tanh(tf.layers.batch_normalization(p_rep))
+        encoded_question = tf.nn.dropout(encoded_question, self.dropout)
+        encoded_hypothesis = tf.nn.dropout(encoded_hypothesis, self.dropout)
+        q_rep = tf.nn.dropout(q_rep, self.dropout)
+        p_rep = tf.nn.dropout(p_rep, self.dropout)
+        return encoded_question, encoded_hypothesis, q_rep, p_rep
+
+
+class Decoder(object):
+
+    def __init__(self, hidden_size, Ddim, dropout, initializer=lambda: None):
+        self.hidden_size = hidden_size
+        self.init_weights = initializer
+        self.Ddim = Ddim
+        self.dropout = dropout
+
+    def run_match_lstm(self, encoded_rep, masks, return_sequence=False):
+        encoded_question, encoded_hypothesis = encoded_rep
+        masks_question, masks_hypothesis = masks
+        masks_hypothesis = tf.reduce_sum(masks_hypothesis, -1)
+        #masks_question = tf.reduce_sum(masks_question, -1)
+        match_lstm_cell_attention_fn = lambda curr_input, state: tf.concat(
+            [curr_input, state], axis=-1)
+
+        query_depth = encoded_question.get_shape().as_list()[-1]
+
+        # output attention is false because we want to output the cell output
+        # and not the attention values
+        with tf.variable_scope("match_lstm_attender"):
+            # attention_mechanism_fw = SeqMatchSeqAttention(
+            #     query_depth, encoded_question, masks_question)
+            # mLSTM_fw_cell = SeqMatchSeqWrapper(tf.contrib.rnn.BasicLSTMCell(self.hidden_size, state_is_tuple=True),
+            #                                    attention_mechanism_fw)
+            # attention_mechanism_bw = SeqMatchSeqAttention(
+            #     query_depth, encoded_question, masks_question)
+            # mLSTM_bw_cell = SeqMatchSeqWrapper(tf.contrib.rnn.BasicLSTMCell(self.hidden_size, state_is_tuple=True),
+            #                                    attention_mechanism_bw)
+            # (output_attender_fw, output_attender_bw), (state_attender_fw, state_attender_bw) = tf.nn.bidirectional_dynamic_rnn(
+            #     mLSTM_fw_cell,
+            #     mLSTM_bw_cell,
+            #     encoded_hypothesis,
+            #     sequence_length=masks_hypothesis,
+            #     dtype=tf.float32
+            # )
+
+            # matchlstm_fw_cell = matchLSTMcell(query_depth, self.hidden_size, encoded_question,
+            #                                   masks_question)
+            # matchlstm_bw_cell = matchLSTMcell(query_depth, self.hidden_size, encoded_question,
+            #                                   masks_question)
+            # (output_attender_fw, output_attender_bw), _ = tf.nn.bidirectional_dynamic_rnn(
+            #     matchlstm_fw_cell,
+            #     matchlstm_bw_cell,
+            #     encoded_hypothesis,
+            #     sequence_length=masks_hypothesis,
+            #     dtype=tf.float32
+            # )
+            cell_fw = MatchLSTMAttnCell(self.hidden_size, encoded_question, masks_question)
+            cell_bw = MatchLSTMAttnCell(self.hidden_size, encoded_question, masks_question)
+            (output_attender_fw, output_attender_bw), _ = tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw,
+                                                                                          inputs=encoded_hypothesis,
+                                                                                          sequence_length=masks_hypothesis,
+                                                                                          dtype=tf.float32)
+        if return_sequence:
+            output_attender = tf.concat(
+                [output_attender_fw, output_attender_bw], -1)
+        else:
+            output_attender = tf.concat(
+                [state_attender_fw[0].h, state_attender_bw[0].h], axis=-1)  # (-1, 2*H)
+        output_attender = tf.tanh(
+            tf.layers.batch_normalization(output_attender))
+        output_attender = tf.nn.dropout(output_attender, self.dropout)
+        return output_attender
+
+    def run_answer_ptr(self, output_attender, masks, scope="ans_ptr"):
+        with tf.variable_scope(scope):
+            batch_size = tf.shape(output_attender)[0]
+            masks_question, masks_passage = masks
+            #masks_passage = tf.reduce_sum(masks_passage, -1)
+            fake_inputs = tf.zeros((batch_size, 2, 1))
+            fake_sequence_length = tf.ones(
+                (batch_size,), dtype=tf.int32) * 2  # 2 for start and end
+
+            answer_ptr_cell_input_fn = lambda curr_input, context: context
+            query_depth_answer_ptr = output_attender.get_shape()[-1]
+
+            with tf.variable_scope("answer_ptr_attender"):
+                with tf.variable_scope("fw"):
+                    answer_ptr_cell_fw = PointerNetLSTMCell(
+                        self.hidden_size, output_attender, masks_passage)
+                    outputs_fw, _ = custom_dynamic_rnn(answer_ptr_cell_fw,
+                                                      fake_inputs,
+                                                      fake_sequence_length,
+                    )
+                with tf.variable_scope("bw"):
+                    answer_ptr_cell_bw = PointerNetLSTMCell(
+                        self.hidden_size, output_attender, masks_passage)
+                    outputs_bw, _ = custom_dynamic_rnn(answer_ptr_cell_bw,
+                                                      fake_inputs,
+                                                      fake_sequence_length,
+                    )
+            start_prob = (outputs_fw[0:, 0, 0:] + outputs_bw[0:, 1, 0:]) / 2
+            end_prob = (outputs_fw[0:, 1, 0:] + outputs_bw[0:, 0, 0:]) / 2
+
+        return start_prob, end_prob
+
+    def run_projection(self, state_attender, output_dim, scope="projection"):
+        with tf.variable_scope(scope):
+            input_projection = state_attender
+            for n, ddim in enumerate(self.Ddim):
+                _, curr_dim = input_projection.get_shape()
+                w = tf.get_variable(dtype=tf.float32,
+                                    shape=[curr_dim, self.hidden_size * ddim],
+                                    name='w_{}'.format(n))
+                b = tf.get_variable(dtype=tf.float32,
+                                    shape=[self.hidden_size * ddim],
+                                    name='b_{}'.format(n))
+                input_projection = tf.matmul(input_projection, w) + b
+
+            _, curr_dim = input_projection.get_shape()
+            output_projection = tf.layers.dense(input_projection,
+                                                output_dim,
+                                                name="projection_final")
+        return output_projection
+
+    def decode(self, encoded_rep, q_rep, masks):
+        """
+        takes in encoded_rep
+        and output a probability estimation over
+        all paragraph tokens on which token should be
+        the start of the answer span, and which should be
+        the end of the answer span.
+        :param encoded_rep: 
+        :param masks
+        :param labels
+        :return: logits: for each word in hypothesis the probability that it is the start word and end word.
+        """
+
+        output_attender = self.run_match_lstm(encoded_rep, masks, True)
+        #output_cnn = cnnsum(output_attender, self.dropout)
+        output_maxpool = tf.reduce_max(output_attender, 1)
+        logits = self.run_projection(output_maxpool, 1, "SemEval_projection")
+        logits_SNLI = self.run_projection(output_maxpool, 3, "SNLI_projection")
+        logits_SQUAD = self.run_answer_ptr(
+            output_attender, masks, "SQUAD_ans_ptr")
+        return logits, logits_SNLI, logits_SQUAD
+
+
+
 class MatchLSTM(object):
 
     def __init__(self, flags, vocab, char_vocab, word_embedding):
